b28d20f2323a967183e33733cb4af3875283217d
philipjk <filippo92galli@gmail.com>
2021-05-07 19:02:11 +0200

    multiprimeRSA.c: make it a littler nicer

--- session3/multiprimeRSA.c
+++ session3/multiprimeRSA.c
@@ -1,15 +1,20 @@
 #include <openssl/bn.h>
+#include <string.h>
 
 void dump_buf(const char *text, unsigned char *buf, int buf_size) {
     printf("%s", text);
     for (int i=0; i<=buf_size; i++) {
         printf("%02x:", buf[i]);
     }
-    printf("\n");
+    printf("]\n");
 }
 
+int main (int argc, const char *argv[]) {
 
-int main (void) {
+    if (argc != 2) {
+        printf("Usage: multiprimeRSA <(numeric) message>\n");
+        exit(0);
+    }
     BN_CTX *ctx = BN_CTX_new();
 
     // find three primes
@@ -22,7 +27,7 @@ int main (void) {
     BN_generate_prime_ex(ret2, size, 1, NULL, NULL, NULL);
     BN_generate_prime_ex(ret3, size, 1, NULL, NULL, NULL);
 
-    // find modulus
+    // find modulus a.k.a. the first part of the public key
     BIGNUM *m = BN_new();
     BN_mul(m, ret1, ret2, ctx);
     BN_mul(m, m, ret3, ctx);
@@ -51,25 +56,28 @@ int main (void) {
     BIGNUM *lambda = BN_new();
     BN_div(lambda, NULL, prod, gcd, ctx);
 
-    // set exponent
+    // set exponent a.k.a. the second part of the public key
     BIGNUM *e = BN_new();
     const char *exp_string = "3";
     BN_dec2bn(&e, exp_string);
 
-    // find modular inverse of e modulo lambda
+    // find modular inverse of e modulo lambda (basically the private key)
     BIGNUM *d = BN_new();
     BN_mod_inverse(d, e, lambda, ctx);
 
     // encrypt
     BIGNUM *encrypted = BN_new();
     BIGNUM *message = BN_new(); // content will be 100
-    const char *message_str = "200";
+    const char *message_str = argv[1];
+    printf("Cleartext message: [%s]\n", message_str);
     BN_dec2bn(&message, message_str);
     BN_mod_exp(encrypted, message, e, m, ctx);
 
     // decrypt
+    char *encrypted_str = BN_bn2hex(encrypted);
+    dump_buf("Ciphertext message: [", (unsigned char *) encrypted_str, strlen(encrypted_str));
     BN_mod_exp(encrypted, encrypted, d, m, ctx);
     char *result = BN_bn2dec(encrypted);
-    printf("%s\n", result);
+    printf("Decrypted message: [%s]\n", result);
 
 }
