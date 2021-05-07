898039083051166f4dc0238b6f9fdb77278669c5
philipjk <filippo92galli@gmail.com>
2021-05-07 18:18:56 +0200

    working prototype of RSA from scratch

--- session3/multiprimeRSA.c
+++ session3/multiprimeRSA.c
@@ -11,65 +11,65 @@ void dump_buf(const char *text, unsigned char *buf, int buf_size) {
 
 int main (void) {
     BN_CTX *ctx = BN_CTX_new();
-    
-    // find three primes
-    BIGNUM *ret1 = NULL;
-    BIGNUM *ret2 = NULL;
-    BIGNUM *ret3 = NULL;
 
-    BN_generate_prime_ex(ret1, 1024, 1, NULL, NULL, NULL);
-    BN_generate_prime_ex(ret2, 1024, 1, NULL, NULL, NULL);
-    BN_generate_prime_ex(ret3, 1024, 1, NULL, NULL, NULL);
+    // find three primes
+    BIGNUM *ret1 = BN_new();
+    BIGNUM *ret2 = BN_new();
+    BIGNUM *ret3 = BN_new();
+
+    int size = 256;
+    BN_generate_prime_ex(ret1, size, 1, NULL, NULL, NULL);
+    BN_generate_prime_ex(ret2, size, 1, NULL, NULL, NULL);
+    BN_generate_prime_ex(ret3, size, 1, NULL, NULL, NULL);
 
     // find modulus
-    BIGNUM *m = NULL;
+    BIGNUM *m = BN_new();
     BN_mul(m, ret1, ret2, ctx);
     BN_mul(m, m, ret3, ctx);
 
     // find LCM
     // first find primes - 1
     const BIGNUM *one = BN_value_one();
-    BIGNUM *f1 = NULL;
-    BIGNUM *f2 = NULL;
-    BIGNUM *f3 = NULL;
+    BIGNUM *f1 = BN_new();
+    BIGNUM *f2 = BN_new();
+    BIGNUM *f3 = BN_new();
     BN_sub(f1, ret1, one);
     BN_sub(f2, ret2, one);
     BN_sub(f3, ret3, one);
 
     // find f1 x f2 x f3
-    BIGNUM *prod = NULL;
+    BIGNUM *prod = BN_new();
     BN_mul(prod, f1, f2, ctx);
     BN_mul(prod, prod, f3, ctx);
-    
+
     // find gcd
-    BIGNUM *gcd = NULL;
+    BIGNUM *gcd = BN_new();
     BN_gcd(gcd, f1, f2, ctx);
     BN_gcd(gcd, gcd, f3, ctx);
 
-    // find LCM
-    BIGNUM *lambda = NULL;
+    // now find LCM
+    BIGNUM *lambda = BN_new();
     BN_div(lambda, NULL, prod, gcd, ctx);
-    
+
     // set exponent
-    BIGNUM **e = NULL;
+    BIGNUM *e = BN_new();
     const char *exp_string = "3";
-    BN_dec2bn(e, exp_string);
+    BN_dec2bn(&e, exp_string);
 
     // find modular inverse of e modulo lambda
-    BIGNUM *d = NULL;
-    BN_mod_inverse(d, *e, lambda, ctx);
+    BIGNUM *d = BN_new();
+    BN_mod_inverse(d, e, lambda, ctx);
 
     // encrypt
-    BIGNUM *encrypted = NULL;
-    BIGNUM **message = NULL; // content will be 100
-    const char *message_str = "100";
-    BN_dec2bn(message, message_str);
-    BN_mod_exp(encrypted, *message, *e, m, ctx);
+    BIGNUM *encrypted = BN_new();
+    BIGNUM *message = BN_new(); // content will be 100
+    const char *message_str = "200";
+    BN_dec2bn(&message, message_str);
+    BN_mod_exp(encrypted, message, e, m, ctx);
 
     // decrypt
-    BN_mod_exp(encrypted, *message, d, m, ctx);
+    BN_mod_exp(encrypted, encrypted, d, m, ctx);
     char *result = BN_bn2dec(encrypted);
     printf("%s\n", result);
-    
-    
+
 }
